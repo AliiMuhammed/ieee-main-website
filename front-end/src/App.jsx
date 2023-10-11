ff5f09abfc49c806dfa12281e734913095825cbe
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-10-11 11:09:52 +0300

    add main style

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Navbar from "./Shared/components/Navbar";
+import Footer from "./Shared/components/Footer";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
 import Router from "./Router";
-import './index.css';
-
+import "./Style/index.css";
+import "@fontsource/cairo";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={Router} />);
-

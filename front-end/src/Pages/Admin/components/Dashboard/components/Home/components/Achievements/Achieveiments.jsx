fb8b6776cfed5d48dc14d5aefaeb668cff6137e1
zeyadamr8 <96020833+ZeyadAmr88@users.noreply.github.com>
2023-10-14 19:33:13 +0300

    create style, logic of first model of Achievs Page

--- front-end/src/Pages/Admin/components/Dashboard/components/Home/components/Achievements/Achieveiments.jsx
+++ front-end/src/Pages/Admin/components/Dashboard/components/Home/components/Achievements/Achieveiments.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./style/achievements.css";
+
 const Achieveiments = () => {
-  return <div>Zeyad</div>;
+
+  const events = useRef("");
+  const members = useRef("");
+  const awards = useRef("");
+
+  const achievload = (event) => {
+    event.preventDefault();
+    console.log(events.current.value);
+  };
+
+  return (
+    <form onSubmit={(e) => achievload(e)}>
+      <div className="achievCards" >
+
+        <div className="achievCard achievEventsCard">
+          <div className="achievLogo"></div>
+          <label className="achievTitle" htmlFor="events">Events</label>
+          <input id="events" type="number" required ref={events} />
+        </div>
+
+        <div className="achievCard achievMembersCard">
+          <div className="achievLogo"></div>
+          <label className="achievTitle" htmlFor="members" >Members</label>
+          <input id="members" type="number" required ref={members}  />
+        </div>
+
+        <div className="achievCard achievAwardsCard">
+          <div className="achievLogo"></div>
+          <label className="achievTitle" htmlFor="awards">Awards</label>
+          <input id="awards" type="number" required ref={awards}  />
+        </div>
+    
+      </div>
+      <button>Save</button>
+    </form>
+  )
 };
 
 export default Achieveiments;

1c9fcdc664557d0ec300c747073c048ff42b896a
yaseenhamdy <yaseenhamdy74@gmail.com>
2023-10-20 00:01:15 +0300

    Add family

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,5 +4,8 @@ import { RouterProvider } from "react-router";
 import Router from "./Router";
 import "./Style/index.css";
 import "@fontsource/cairo";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={Router} />);
